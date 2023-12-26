536f10daa1f4c6f69ca4a8af59742545dc6a95c9
voityvaiti <flashlight@i.ua>
2023-12-26 22:13:02 +0200

    Init

---
+++ src/main/java/com/myproject/blogwebservice/BlogWebServiceApplication.java
@@ -0,0 +1,13 @@
+package com.myproject.blogwebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogWebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogWebServiceApplication.class, args);
+	}
+
+}
