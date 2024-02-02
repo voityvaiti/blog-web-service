c1e89758459669b2f994d4967fe738c6b7490753
voityvaiti <flashlight@i.ua>
2024-02-03 00:14:38 +0200

    AppUser entity created

---
+++ src/main/java/com/myproject/blogwebservice/entity/AppUser.java
@@ -0,0 +1,40 @@
+package com.myproject.blogwebservice.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Table(name = "usr")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AppUser {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "username", unique = true)
+    private String username;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "role")
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Column(name = "nickname")
+    private String nickname;
+
+    @OneToMany(mappedBy = "user")
+    private List<Post> posts = new ArrayList<>();
+
+
+}
