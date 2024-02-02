7ca07a2bde8ccd112fdeac85759e7a900d2b5937
voityvaiti <flashlight@i.ua>
2024-02-03 00:39:12 +0200

    Used @Data annotation in Post entity

--- src/main/java/com/myproject/blogwebservice/entity/Post.java
+++ src/main/java/com/myproject/blogwebservice/entity/Post.java
@@ -5,20 +5,17 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
-import lombok.Getter;
+import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 import java.time.LocalDateTime;
-import java.util.Objects;
 import java.util.UUID;
 
 @Entity
 @Table(name = "post")
+@Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Getter
-@Setter
 public class Post {
 
     @Id
@@ -43,26 +40,4 @@ public class Post {
     @NotNull
     private AppUser user;
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Post post = (Post) o;
-        return Objects.equals(id, post.id) && Objects.equals(title, post.title) && Objects.equals(article, post.article) && Objects.equals(publicationDateTime, post.publicationDateTime);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, title, article, publicationDateTime);
-    }
-
-    @Override
-    public String toString() {
-        return "Post{" +
-                "id=" + id +
-                ", title='" + title + '\'' +
-                ", article='" + article + '\'' +
-                ", publicationDateTime=" + publicationDateTime +
-                '}';
-    }
 }
