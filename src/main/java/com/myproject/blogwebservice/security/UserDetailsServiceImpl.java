22e718d4218c913d4fe795821932583517002318
voityvaiti <flashlight@i.ua>
2024-02-05 01:10:33 +0200

    UserDetailsServiceImpl created

---
+++ src/main/java/com/myproject/blogwebservice/security/UserDetailsServiceImpl.java
@@ -0,0 +1,38 @@
+package com.myproject.blogwebservice.security;
+
+import com.myproject.blogwebservice.entity.AppUser;
+import com.myproject.blogwebservice.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        AppUser appUser = userRepository.findByUsername(username).orElseThrow(
+                () -> new UsernameNotFoundException("User not found with username: " + username)
+        );
+
+        return new User(
+                appUser.getUsername(),
+                appUser.getPassword(),
+                List.of(
+                        new SimpleGrantedAuthority(appUser.getRole().name())
+                )
+        );
+    }
+
+}
