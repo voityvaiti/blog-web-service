8a940fae4421be8efb93452409cd740fb8fba5f2
voityvaiti <flashlight@i.ua>
2024-02-07 23:14:06 +0200

    JwtRequest and JwtResponse dtos created

---
+++ src/main/java/com/myproject/blogwebservice/dto/JwtRequestDto.java
@@ -0,0 +1,16 @@
+package com.myproject.blogwebservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class JwtRequestDto {
+
+    private String username;
+
+    private String password;
+
+}
---
+++ src/main/java/com/myproject/blogwebservice/dto/JwtResponseDto.java
@@ -0,0 +1,14 @@
+package com.myproject.blogwebservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class JwtResponseDto {
+
+    private String token;
+
+}
