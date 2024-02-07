5763777e7664040cdc868f91e721ce742ee999e2
voityvaiti <flashlight@i.ua>
2024-02-07 20:04:28 +0200

    AuthenticationManager bean added to WebSecurityConfig

--- src/main/java/com/myproject/blogwebservice/security/WebSecurityConfig.java
+++ src/main/java/com/myproject/blogwebservice/security/WebSecurityConfig.java
@@ -2,6 +2,8 @@ package com.myproject.blogwebservice.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -47,6 +49,11 @@ public class WebSecurityConfig {
         source.registerCorsConfiguration("/**", configuration);
 
         return source;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
     }
 
     @Bean
