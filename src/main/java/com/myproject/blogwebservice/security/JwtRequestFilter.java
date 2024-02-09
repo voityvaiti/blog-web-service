0b6e6624ea76b595ab4fa01fde6c73d3013f243e
voityvaiti <flashlight@i.ua>
2024-02-09 22:28:32 +0200

    Filter to read token and set authentication to SecContext

---
+++ src/main/java/com/myproject/blogwebservice/security/JwtRequestFilter.java
@@ -0,0 +1,56 @@
+package com.myproject.blogwebservice.security;
+
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.security.SignatureException;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class JwtRequestFilter extends OncePerRequestFilter {
+
+    private final JwtTokenUtil jwtTokenUtil;
+
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        String authHeader = request.getHeader("Authorization");
+        String username = null;
+        String jwt = null;
+
+        if (authHeader != null && authHeader.startsWith("Bearer ")) {
+            jwt = authHeader.substring(7);
+
+            try {
+                username = jwtTokenUtil.getUsername(jwt);
+
+            } catch (ExpiredJwtException | SignatureException e) {
+                e.printStackTrace();
+            }
+        }
+
+        if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+
+            UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(
+                    username,
+                    null,
+                    jwtTokenUtil.getRoles(jwt).stream().map(SimpleGrantedAuthority::new).toList()
+            );
+
+            SecurityContextHolder.getContext().setAuthentication(token);
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
