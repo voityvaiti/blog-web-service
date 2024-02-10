cbc97ec1dee489fb6965b7c17cc6ed723d2a4cde
voityvaiti <flashlight@i.ua>
2024-02-10 13:42:06 +0200

    Using AuthService in AuthController

--- src/main/java/com/myproject/blogwebservice/controller/AuthController.java
+++ src/main/java/com/myproject/blogwebservice/controller/AuthController.java
@@ -2,14 +2,9 @@ package com.myproject.blogwebservice.controller;
 
 import com.myproject.blogwebservice.dto.JwtRequestDto;
 import com.myproject.blogwebservice.dto.JwtResponseDto;
-import com.myproject.blogwebservice.security.JwtTokenUtil;
+import com.myproject.blogwebservice.service.abstraction.AuthService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,22 +15,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class AuthController {
 
-    private final UserDetailsService userDetailsService;
-
-    private final JwtTokenUtil jwtTokenUtil;
-
-    private final AuthenticationManager authenticationManager;
-
+    private final AuthService authService;
 
 
     @PostMapping
-    public ResponseEntity<JwtResponseDto> createAuthToken(@RequestBody JwtRequestDto jwtRequestDto) throws BadCredentialsException {
+    public ResponseEntity<JwtResponseDto> createAuthToken(@RequestBody JwtRequestDto jwtRequestDto) {
 
-        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(jwtRequestDto.getUsername(), jwtRequestDto.getPassword()));
+        String token = authService.generateTokenByCredentials(jwtRequestDto.getUsername(), jwtRequestDto.getPassword());
 
-        UserDetails userDetails = userDetailsService.loadUserByUsername(jwtRequestDto.getUsername());
-
-        return ResponseEntity.ok(new JwtResponseDto(jwtTokenUtil.generateToken(userDetails)));
+        return ResponseEntity.ok(new JwtResponseDto(token));
     }
 
 }
