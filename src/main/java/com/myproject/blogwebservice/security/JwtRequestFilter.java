c249a9c6ec0a496115738be81774d29660aa8b9d
voityvaiti <flashlight@i.ua>
2024-02-10 22:04:15 +0200

    constants in JwtRequestFilter

--- src/main/java/com/myproject/blogwebservice/security/JwtRequestFilter.java
+++ src/main/java/com/myproject/blogwebservice/security/JwtRequestFilter.java
@@ -19,18 +19,22 @@ import java.io.IOException;
 @RequiredArgsConstructor
 public class JwtRequestFilter extends OncePerRequestFilter {
 
+    private static final String AUTH_HEADER_KEY = "Authorization";
+    private static final String AUTH_HEADER_VALUE_START = "Bearer ";
+
+
     private final JwtTokenUtil jwtTokenUtil;
 
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
 
-        String authHeader = request.getHeader("Authorization");
+        String authHeader = request.getHeader(AUTH_HEADER_KEY);
         String username = null;
         String jwt = null;
 
-        if (authHeader != null && authHeader.startsWith("Bearer ")) {
-            jwt = authHeader.substring(7);
+        if (authHeader != null && authHeader.startsWith(AUTH_HEADER_VALUE_START)) {
+            jwt = authHeader.substring(AUTH_HEADER_VALUE_START.length());
 
             try {
                 username = jwtTokenUtil.getUsername(jwt);
