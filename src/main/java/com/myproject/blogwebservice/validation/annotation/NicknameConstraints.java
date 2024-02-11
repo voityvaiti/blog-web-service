6062d172df2cd2aa262689cd2edb750ac316caac
voityvaiti <flashlight@i.ua>
2024-02-11 19:09:27 +0200

    Annotations with constraints for some AppUser fields

---
+++ src/main/java/com/myproject/blogwebservice/validation/annotation/NicknameConstraints.java
@@ -0,0 +1,27 @@
+package com.myproject.blogwebservice.validation.annotation;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@NotBlank(message = "Nickname is required.")
+@Size(max = 30, message = "Nickname cannot be longer then 30 characters.")
+
+@Target({ElementType.FIELD, ElementType.METHOD, ElementType.PARAMETER, ElementType.ANNOTATION_TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = {})
+public @interface NicknameConstraints {
+
+    String message() default "Invalid nickname.";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+
+}
---
+++ src/main/java/com/myproject/blogwebservice/validation/annotation/UsernameConstraints.java
@@ -0,0 +1,28 @@
+package com.myproject.blogwebservice.validation.annotation;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+
+@NotBlank(message = "Username is required.")
+@Size(min = 3, max = 20, message = "Username must be between 3 and 20 characters.")
+
+@Target({ElementType.FIELD, ElementType.METHOD, ElementType.PARAMETER, ElementType.ANNOTATION_TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = {})
+public @interface UsernameConstraints {
+
+    String message() default "Invalid username.";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+
+}
