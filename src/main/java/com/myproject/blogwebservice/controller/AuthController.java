de3115522d09363585f1965bf7b5e2cef06b8e08
voityvaiti <flashlight@i.ua>
2024-02-13 13:43:09 +0200

    AuthController signUp endpoint

--- src/main/java/com/myproject/blogwebservice/controller/AuthController.java
+++ src/main/java/com/myproject/blogwebservice/controller/AuthController.java
@@ -2,8 +2,15 @@ package com.myproject.blogwebservice.controller;
 
 import com.myproject.blogwebservice.dto.JwtRequestDto;
 import com.myproject.blogwebservice.dto.JwtResponseDto;
+import com.myproject.blogwebservice.dto.SignUpDto;
+import com.myproject.blogwebservice.entity.AppUser;
+import com.myproject.blogwebservice.exception.UserDuplicationException;
 import com.myproject.blogwebservice.service.abstraction.AuthService;
+import com.myproject.blogwebservice.service.abstraction.UserService;
+import com.myproject.blogwebservice.validation.validator.UserUniquenessValidator;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,13 +24,28 @@ public class AuthController {
 
     private final AuthService authService;
 
+    private final UserService userService;
 
-    @PostMapping
+    private final UserUniquenessValidator userUniquenessValidator;
+
+
+    @PostMapping("/login")
     public ResponseEntity<JwtResponseDto> createAuthToken(@RequestBody JwtRequestDto jwtRequestDto) {
 
         String token = authService.generateTokenByCredentials(jwtRequestDto.getUsername(), jwtRequestDto.getPassword());
 
         return ResponseEntity.ok(new JwtResponseDto(token));
+    }
+
+    @PostMapping("/signup")
+    public ResponseEntity<Void> createUser(@RequestBody @Valid SignUpDto signUpDto) throws UserDuplicationException {
+
+        AppUser user = signUpDto.toAppUser();
+
+        userUniquenessValidator.validate(user);
+
+        userService.createUser(user);
+        return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
 }
