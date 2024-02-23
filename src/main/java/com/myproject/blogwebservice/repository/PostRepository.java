5e0e6eacc8025fa0125c518ee35e2344d235e898
voityvaiti <flashlight@i.ua>
2024-02-23 13:27:32 +0200

    PostRepository findAllPostsByUserId method

--- src/main/java/com/myproject/blogwebservice/repository/PostRepository.java
+++ src/main/java/com/myproject/blogwebservice/repository/PostRepository.java
@@ -4,8 +4,12 @@ import com.myproject.blogwebservice.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface PostRepository extends JpaRepository<Post, UUID> {
+
+    List<Post> findAllByUserId(UUID id);
+
 }
