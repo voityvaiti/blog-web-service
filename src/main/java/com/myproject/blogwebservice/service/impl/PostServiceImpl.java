1e089efb9baad34ce83048049ea9a10f3cf53b48
voityvaiti <flashlight@i.ua>
2024-05-21 13:40:42 +0300

    Logging in PostServiceImpl

--- src/main/java/com/myproject/blogwebservice/service/impl/PostServiceImpl.java
+++ src/main/java/com/myproject/blogwebservice/service/impl/PostServiceImpl.java
@@ -5,6 +5,7 @@ import com.myproject.blogwebservice.exception.ResourceNotFoundException;
 import com.myproject.blogwebservice.repository.PostRepository;
 import com.myproject.blogwebservice.service.abstraction.PostService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -14,6 +15,7 @@ import java.util.List;
 import java.util.UUID;
 
 @Service
+@Slf4j
 @RequiredArgsConstructor
 public class PostServiceImpl implements PostService {
 
@@ -22,26 +24,37 @@ public class PostServiceImpl implements PostService {
 
     @Override
     public List<Post> getAll() {
+
+        log.debug("Returning Post list");
+
         return postRepository.findAll();
     }
 
     @Override
     public Page<Post> getAll(Pageable pageable) {
+        log.debug("Returning Post page");
+
         return postRepository.findAll(pageable);
     }
 
     @Override
     public Page<Post> getAllByUserId(UUID id, Pageable pageable) {
+        log.debug("Returning Post page by User ID: {}", id);
+
         return postRepository.findAllByUserId(id, pageable);
     }
 
     @Override
     public Post getById(UUID id) {
+        log.debug("Looking for Post with ID: {}", id);
+
         return postRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Post not found with ID: " + id));
     }
 
     @Override
     public boolean isPostOfUser(UUID id, String username) {
+        log.debug("Checking if Post with ID {} belongs to User with username: {}", id, username);
+
         return postRepository.existsByIdAndUserUsername(id, username);
     }
 
@@ -50,11 +63,15 @@ public class PostServiceImpl implements PostService {
 
         post.setPublicationDateTime(LocalDateTime.now());
 
+        log.debug("Saving new Post: {}", post);
+
         return postRepository.save(post);
     }
 
     @Override
     public void delete(UUID id) {
+
+        log.debug("Looking for Post to delete with ID: {}", id);
 
         postRepository.findById(id)
                 .ifPresentOrElse(
