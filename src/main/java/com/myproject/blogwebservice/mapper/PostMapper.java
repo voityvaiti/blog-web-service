241c91b96b13c970ce6c5657ecbdc83ae7db7bc1
voityvaiti <flashlight@i.ua>
2024-05-27 17:21:51 +0300

    Mapping publicationDateTime to creteadDate

--- src/main/java/com/myproject/blogwebservice/mapper/PostMapper.java
+++ src/main/java/com/myproject/blogwebservice/mapper/PostMapper.java
@@ -4,11 +4,25 @@ import com.myproject.blogwebservice.dto.request.PostRequestDto;
 import com.myproject.blogwebservice.dto.response.PostResponseDto;
 import com.myproject.blogwebservice.entity.Post;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 
 @Mapper
 public interface PostMapper {
+
+    @Mapping(source = "createdDate", target = "publicationDateTime")
     PostResponseDto mapToPostResponseDto(Post post);
 
     Post mapToPost(PostRequestDto postRequestDto);
+
+
+    default LocalDateTime map(ZonedDateTime zonedDateTime) {
+        return zonedDateTime != null
+                ? zonedDateTime.withZoneSameInstant(ZoneId.systemDefault()).toLocalDateTime()
+                : null;
+    }
 
 }
