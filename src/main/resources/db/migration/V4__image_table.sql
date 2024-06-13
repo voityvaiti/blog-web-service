a3f9fd305ba72c0528dfa513cd150246e9f35032
voityvaiti <flashlight@i.ua>
2024-06-13 17:39:44 +0300

    Image table auditing columns added

--- src/main/resources/db/migration/V4__image_table.sql
+++ src/main/resources/db/migration/V4__image_table.sql
@@ -3,5 +3,7 @@ CREATE TABLE images
     id   UUID DEFAULT gen_random_uuid(),
     name VARCHAR(255),
     url  VARCHAR(255),
+    created_date TIMESTAMPTZ,
+    updated_date TIMESTAMPTZ,
     PRIMARY KEY (id)
 );
