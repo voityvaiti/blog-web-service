df6f9ab94eb5f46ab0cd0159cc400169fcca2753
voityvaiti <flashlight@i.ua>
2024-06-16 13:50:56 +0300

    /upload-avatar endpoint in UserController

--- src/main/java/com/myproject/blogwebservice/controller/UserController.java
+++ src/main/java/com/myproject/blogwebservice/controller/UserController.java
@@ -3,6 +3,7 @@ package com.myproject.blogwebservice.controller;
 import com.myproject.blogwebservice.dto.response.UserResponseDto;
 import com.myproject.blogwebservice.entity.AppUser;
 import com.myproject.blogwebservice.mapper.UserMapper;
+import com.myproject.blogwebservice.service.abstraction.AvatarService;
 import com.myproject.blogwebservice.service.abstraction.UserService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -11,9 +12,9 @@ import org.mapstruct.factory.Mappers;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
 
 @RestController
 @RequestMapping("${api-prefix}/users")
@@ -22,6 +23,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     private final UserService userService;
+    private final AvatarService avatarService;
 
     private final UserMapper userMapper = Mappers.getMapper(UserMapper.class);
 
@@ -30,12 +32,24 @@ public class UserController {
     @GetMapping("/current")
     public ResponseEntity<UserResponseDto> getCurrent(Authentication authentication) {
 
-        if(authentication == null) {
+        if (authentication == null) {
             return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
         }
         AppUser currentUser = userService.getByUsername(authentication.getName());
 
         return ResponseEntity.ok(userMapper.mapToUserResponseDto(currentUser));
+    }
+
+    @PostMapping("/upload-avatar")
+    public ResponseEntity<Void> uploadAvatar(@RequestParam("file") MultipartFile file, Authentication authentication) {
+
+        if (file == null || file.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        avatarService.uploadAvatar(authentication.getName(), file);
+
+        return ResponseEntity.ok().build();
+
     }
 
 }
